d9cbd503ce11a9664030041410cddbb568dae679
patricia1602 <80828415+patricia1602@users.noreply.github.com>
2024-03-26 11:26:14 -0300

    aula 02

---
+++ POO/src/aula02/Aula02.java
@@ -0,0 +1,26 @@
+package aula02;
+
+public class Aula02 {
+
+    public static void main(String[] args) {
+
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.tampar();
+        // c1.destampar();
+
+        c1.status();
+        c1.rabiscar();
+
+        Caneta c2 = new Caneta();
+        c2.modelo = "Compactor";
+        c2.cor = "Vermelha";
+        c2.ponta = 0.5f;
+        c2.destampar();
+        c2.status();
+        c2.rabiscar();
+
+    }
+
+}
