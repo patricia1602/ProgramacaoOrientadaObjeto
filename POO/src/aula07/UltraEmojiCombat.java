aba0953e9456aa43df952ef7a38db7b999ad11f5
patricia1602 <80828415+patricia1602@users.noreply.github.com>
2024-04-01 15:34:32 -0300

    Aula 07 luta

--- POO/src/aula07/UltraEmojiCombat.java
+++ POO/src/aula07/UltraEmojiCombat.java
@@ -31,8 +31,8 @@ public class UltraEmojiCombat {
                 Luta UEC01 = new Luta();
                 UEC01.marcarLuta(l[0], l[1]);
                 UEC01.lutar();
-                l[0].status();
-                l[1].status();
+                //l[0].status();
+                //l[1].status();
 
         }
 }
