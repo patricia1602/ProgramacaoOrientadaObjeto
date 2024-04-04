fd27cf18276bd3ed3070dd37820d4f994046bf50
patricia1602 <80828415+patricia1602@users.noreply.github.com>
2024-04-04 11:26:46 -0300

    Aula013_PolimorfismoSobrecarga

---
+++ POO/src/aula013/Aula013.java
@@ -0,0 +1,11 @@
+package aula013;
+
+public class Aula013 {
+    // Programa principal
+    public static void main(String[] args) {
+        Cachorro c = new Cachorro();
+        
+        c.reagir(2, 12.5f);
+        c.reagir(17, 4.5f);
+    }
+}
